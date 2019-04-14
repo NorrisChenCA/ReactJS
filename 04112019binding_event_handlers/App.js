591464fe5ecb3d22f2f927b686a035964aed3304
martin <martin@zination.com>
2019-04-13 22:58:23 -0400

    binding event handlers

---
+++ 04112019binding_event_handlers/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import EventBind from '/EventBind';
+
+
+class App extends Component {
+	render() {
+		return(
+			<div className="App">
+				<EventBind />
+			</div>
+		)
+	}
+}
+
+export default App;
