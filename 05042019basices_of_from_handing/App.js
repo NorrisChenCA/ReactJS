798e148f456e0607043d6c8bf42d724275507906
martin <martonlineca@gmail.com>
2019-05-13 22:56:28 -0400

    mac

--- 05042019basices_of_from_handing/App.js
+++ 05042019basices_of_from_handing/App.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
-import './App.css';
 
-import Welcome from '/Welcome';
-
+import form from '/Form';
 
 class App extends Component {
 	render() {
 		return(
 			<div className="App">
-				<Welcome />
+				<form/>
 			</div>
 		)
 	}
